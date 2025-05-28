c47f1bca0d69c343fb55a54d02cf26464902a636
vanxhu <sk88824924@gmail.com>
2025-05-28 13:49:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -93,10 +93,10 @@ IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/dreamcinezone')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/dreamcinezone')
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/dreamcinezone')
-DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK = environ.get('DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/dreamcinezone')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/apna_movie_Hq')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Anime_All_Hindi_In_Crunchyroll')
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Og_Vanxhuu')
+DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK = environ.get('DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/Anime_All_Hindi_In_Crunchyroll')
 OWNERID = int(os.environ.get('OWNERID', '7527314266'))  # Replace with the actual admin ID
 
 # ============================
