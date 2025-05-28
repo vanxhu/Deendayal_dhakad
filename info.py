29dfb44cad15f5b3699d2f1817c761a9580f7b4c
vanxhu <sk88824924@gmail.com>
2025-05-28 12:51:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,9 +61,9 @@ OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'deendayaldhaked@ybl')
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "@cluster0.iscxsme.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_URI2 = environ.get('DATABASE_URI2',"mongodb+srv://sk88824924:JfNR49xOU6rgzINf@cluster0.m0tbs2c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "sudipsaha2006")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sk88824924:JfNR49xOU6rgzINf@cluster0.m0tbs2c.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI2 = environ.get('DATABASE_URI2',"mongodb+srv://sk88824924:JfNR49xOU6rgzINf@cluster0.m0tbs2c.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "JfNR49xOU6rgzINf")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Deendayal_files')
 
 # ============================
